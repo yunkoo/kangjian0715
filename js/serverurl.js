a8825acbd1f2205dffa6e61c5347109dc7123c28
yunkoo <1838458141@qq.com>
2015-07-15 10:40:23 +0800

    2

--- js/serverurl.js
+++ js/serverurl.js
@@ -1,6 +1,6 @@
 
 
-var SERVER = 'http://zjgcd.sqapp.cn/index.php';
-var SERVER_ROOT = 'http://zjgcd.sqapp.cn/';
-var IMG_SERVER_ROOT = 'http://zjgcd.sqapp.cn/';
+var SERVER = 'http://kangjian.sqapp.cn/index.php';
+var SERVER_ROOT = 'http://kangjian.sqapp.cn/';
+var IMG_SERVER_ROOT = 'http://kangjian.sqapp.cn/';
 
